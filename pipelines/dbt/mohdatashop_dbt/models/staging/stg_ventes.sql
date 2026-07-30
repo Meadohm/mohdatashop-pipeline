4ec2db4065bb4bd5cd501367b2f36eb7b8743ec8
Mohamed Fofana <fofanamohamed7927@gmail.com>
2026-07-30 21:30:58 +0000

    feat(dbt): init projet dbt + premier modele staging stg_ventes

---
+++ pipelines/dbt/mohdatashop_dbt/models/staging/stg_ventes.sql
@@ -0,0 +1,11 @@
+select
+    p.id as produit_id,
+    p.nom as produit,
+    c.nom as categorie,
+    sum(lc.quantite) as quantite_vendue,
+    sum(lc.quantite * lc.prix_unitaire) as chiffre_affaires
+from {{ source('postgres', 'lignes_commande') }} lc
+join {{ source('postgres', 'produits') }} p on p.id = lc.produit_id
+join {{ source('postgres', 'categories') }} c on c.id = p.categorie_id
+group by p.id, p.nom, c.nom
+order by chiffre_affaires desc
